46981f813780448dcf681f0d1c7d5b4d4c726175
Michael Kienbsuch <mkienbus@gmail.com>
2022-03-31 17:42:32 -0600

    reservations from back end are: 1) specific to user correctly 2)retrieving restaurant name correctly 3)retrieving reservation date/time correctly. Need to format onto page properly

--- src/components/ReservationCalendar.js
+++ src/components/ReservationCalendar.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
 //imported Calendar from tool
 import Calendar from "react-calendar";
+import moment from "moment";
 import 'react-calendar/dist/Calendar.css';
 
 //calendar resource and info on customizing CSS
 //https://blog.logrocket.com/react-calendar-tutorial-build-customize-calendar/
 
+//stretch different calendar? can't change data type with current Calendar
+//https://mui.com/components/date-picker/
+
 //exported Component to be rendered by ReservationModal
 function ReservationCalendar({callBackClickedDate}){
-    const [date, setDate] = useState(new Date());
 
-    //moment library or JS
+    // let formattedDate = (moment(new Date()).format("MMM Do YY"))
+    
+    const [date, setDate] = useState(new Date())
+
+    // moment().format("MMM Do YY"); 
+
     //callback to ReservationModal for date information that was selected
+
+    let formattedDate = date.toDateString();
 
     return(
     <>
@@ -21,7 +31,7 @@ function ReservationCalendar({callBackClickedDate}){
             </div>
             <p className='text-center'>
                 <span className='bold'>Selected Date:</span>{' '}
-                {date.toDateString()}
+                {date.toLocaleDateString()}
             </p>
         </div>
     </>
--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -4,21 +4,38 @@ import { useState, useEffect } from "react"
 
 
 function Reservations(){
-    const [reservations, setReservations] = useState([])
+    const [reservationName, setReservationName] = useState([])
+    const [reservationDate, setReservationDate] = useState([])
 
+    //.map over response 
     useEffect(() => {
         fetch('/reservations')
             .then(r => r.json())
-            .then(data => console.log(data));
+            .then((data) => {
+                setReservationName(data.map((d) => d.restaurant.name))
+                setReservationDate(data.map((d) => d.reservation_date))
+            })
     },[]);
 
     return(
         <>
             <Nav />
             <h3>Reservations</h3>
-            <h4>{reservations}</h4>
+            <h4>{reservationName}</h4>
+            <br></br>
+            <h4>{reservationDate}</h4>
         </>
     )
 }
 
 export default Reservations;
+
+// const GetData = [];
+//   useEffect(() => {
+//     fetch(API_URL)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         GetModesData.push(...data);
+//         setDataState(GetData.map((d) => d.modeName));
+//       });
+//   }, []);
