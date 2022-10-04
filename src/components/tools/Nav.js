aea6010e8aa8cd825c25dd10ec42a7036438135d
Michael Kienbsuch <mkienbus@gmail.com>
2022-10-04 16:56:03 -0600

    added MUI Button to navbar elements

--- src/components/tools/Nav.js
+++ src/components/tools/Nav.js
@@ -10,10 +10,10 @@ function Nav(){
     return(
 
         <nav id="navbar">
-            <Link to = "/">HOME  |</Link>
+            <Link to = "/"><Button>HOME</Button> </Link>
             {/* <Link to = "/restaurants">  RESTAURANTS  |</Link> */}
-            <Link to = "/reservations">  RESERVATIONS  |</Link>
-            <Link to = "/favorites">  FAVORITES</Link>
+            <Link to = "/reservations">  <Button>RESERVATIONS</Button> </Link>
+            <Link to = "/favorites">  <Button>FAVORITES</Button></Link>
             <br></br>
         </nav>
     )
