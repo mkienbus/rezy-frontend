63ea8230ede792e665780ff0de42efab0a8d7096
Michael Kienbsuch <mkienbus@gmail.com>
2022-10-04 17:34:54 -0600

    set external links to open new window

--- src/components/About.js
+++ src/components/About.js
@@ -10,8 +10,8 @@ function About(){
             <h2>Rezy</h2>
             <p>An application for making reservations at restaurants!</p>
             <p>Created by Michael Kienbusch</p>
-            <p>Github <a href="https://github.com/mkienbus">here</a></p>
-            <p>Dev.to <a href="https://dev.to/mkienbus">here</a></p>
+            <p>Github <a href="https://github.com/mkienbus" target="_blank" rel="noopener noreferrer">here</a></p>
+            <p>Dev.to <a href="https://dev.to/mkienbus" target="_blank" rel="noopener noreferrer">here</a></p>
         </div>
     )
 }
