e356b62c14cea6a28a400de04ac3888a74903107
Michael Kienbusch <82896371+mkienbus@users.noreply.github.com>
2023-04-25 12:50:46 -0600

    added personal portfolio link

--- src/components/About.js
+++ src/components/About.js
@@ -9,6 +9,7 @@ function About(){
             <p>An application for making reservations at restaurants!</p>
             <p>Created by Michael Kienbusch</p>
             <p>Github <a href="https://github.com/mkienbus" target="_blank" rel="noopener noreferrer">here</a></p>
+            <p>Personal Portfolio <a href="https://mkienbus.github.io/" target = "_blank" rel="noopener noreferrer">here</a></p>
             <p>Dev.to <a href="https://dev.to/mkienbus" target="_blank" rel="noopener noreferrer">here</a></p>
         </div>
     )
